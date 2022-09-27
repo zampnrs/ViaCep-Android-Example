876be3e398b5ece70e6c29c2286ea362d15c0c8c
Nicolas Zamprogno <znicolasrs@gmail.com>
2022-09-26 23:09:10 -0300

    Fixing toast showing

--- app/src/main/java/br/zampnrs/viacepapi_example/utils/Extensions.kt
+++ app/src/main/java/br/zampnrs/viacepapi_example/utils/Extensions.kt
@@ -3,12 +3,10 @@ package br.zampnrs.viacepapi_example.utils
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 
-import br.zampnrs.viacepapi_example.R
-
 
 fun Fragment.showToast(text: String, duration: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(activity?.applicationContext,
-        getString(R.string.save_contact_error),
+        text,
         duration
     ).show()
 }
